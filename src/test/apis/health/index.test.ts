1545a695798eca8c1b995d8f46150751c1ebe8e6
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-08-07 22:48:32 +0100

    updated test for health route

--- src/test/apis/health/index.test.ts
+++ src/test/apis/health/index.test.ts
@@ -1,8 +1,25 @@
 import request from "supertest";
 
-import { server } from "../../../server";
+jest.mock("../../../serverEnv", () => ({
+  port: "5000",
+  allowedOrigins: "*",
+  redis: "redis://localhost:6379",
+  googleClientId: "test-id",
+  googleClientSecret: "test-secret",
+  environment: "paytrail-express-backend-test",
+  isProduction: true,
+  databaseUrl: "postgresql://postgres:postgres_123@localhost:5432/paytrail_postgres",
+  baseUrl: "https://test.com",
+  appScheme: "paytrail://",
+}));
+
+import { server, startServer } from "../../../server";
 
 describe("test for the health api", () => {
+  beforeAll(async () => {
+    await startServer();
+  });
+
   test("it should return a 200 status code", async () => {
     const res = await request(server).get("/health").expect("Content-Type", /json/);
 
