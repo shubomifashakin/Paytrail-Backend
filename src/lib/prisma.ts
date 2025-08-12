d0db3a781d34b3f9cdeff26737d2559192a25096
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-08-12 07:52:29 +0100

    removed comments

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient({
   transactionOptions: {
     maxWait: 5000,
-    timeout: 15000, //TODO:use your own configuration
+    timeout: 15000,
   },
 });
 
