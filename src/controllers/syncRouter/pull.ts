18ce225843b4ed631e4d77012b1d184152c4905b
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-08-15 13:31:15 +0100

    bug fix: pull only user data

--- src/controllers/syncRouter/pull.ts
+++ src/controllers/syncRouter/pull.ts
@@ -5,7 +5,9 @@ import prisma from "../../lib/prisma";
 export default async function (req: Request, res: Response) {
   const lastPullDate = (req.query.lastPullDate as string) || null;
 
-  const whereClause = lastPullDate ? { updatedAt: { gte: new Date(lastPullDate) } } : {};
+  const whereClause = lastPullDate
+    ? { updatedAt: { gte: new Date(lastPullDate) }, userId: req.user.id }
+    : { userId: req.user.id };
 
   //find everything from the database newer than the last pull date
   const [logs, categories, paymentMethods, budgets] = await prisma.$transaction([
