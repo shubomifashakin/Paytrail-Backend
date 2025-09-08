7e1b077f620f9fef1ed41c72c53f593b8d06faa0
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-09-08 18:49:34 +0100

    added a get all notifications route

--- src/routes/notifications.ts
+++ src/routes/notifications.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
+import getAllNotifications from "../controllers/notificationsRouter/allNotifications";
 import registerForPushNotifications from "../controllers/notificationsRouter/registerForPushNotifications";
 
 import asyncHandler from "../utils/asyncHandler";
 
 const notificationRouter = Router();
 
+notificationRouter.get("/", asyncHandler(getAllNotifications));
 notificationRouter.post("/register", asyncHandler(registerForPushNotifications));
 
 export default notificationRouter;
