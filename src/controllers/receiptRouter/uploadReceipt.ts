78bdd483dfe4be58c23e76f0591f21fd2c72aa4f
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-09-20 14:36:13 +0100

    setup receipt parser

--- src/controllers/receiptRouter/uploadReceipt.ts
+++ src/controllers/receiptRouter/uploadReceipt.ts
@@ -1,8 +1,10 @@
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { v4 as uuid } from "uuid";
-import z from "zod";
+
+import { FilePart, TextPart } from "ai";
 import { Request, Response } from "express";
 
+import { ReceiptParser } from "../../lib/receiptParser";
 import logger from "../../lib/logger";
 import s3Client from "../../lib/s3Client";
 
@@ -10,6 +12,7 @@ import serverEnv from "../../serverEnv";
 
 import { MESSAGES } from "../../utils/constants";
 import { clearBuffer } from "../../utils/fns";
+import { receiptParseRequestValidator } from "../../utils/validators";
 
 export async function uploadReceipt(req: Request, res: Response) {
   if (!req.files?.length) {
@@ -25,11 +28,7 @@ export async function uploadReceipt(req: Request, res: Response) {
     });
   }
 
-  const { success, error, data } = z
-    .object({
-      privacyMode: z.enum(["true", "false"], { error: "Invalid privacy mode" }),
-    })
-    .safeParse(req.body);
+  const { success, error, data } = receiptParseRequestValidator.safeParse(req.body);
 
   if (!success) {
     logger.warn(MESSAGES.BAD_REQUEST, {
@@ -44,26 +43,85 @@ export async function uploadReceipt(req: Request, res: Response) {
     });
   }
 
-  if (data.privacyMode === "true") {
-    //FIXME: PROCESS ON THE SERVER
-    //PASS THE PAYMENT METHODS(ID & DESC), CATEGORIES(ID & DESC) & THE IMAGES TO THE MODEL
-    //SPECIFY THE OUTPUT THE AI MODEL SHOULD RETURN
-    //GIVE THE AI RULES FOR EXTRACTION
-    //ENSURE THAT THE OUTPUT MATCHES THE EXPECTED SCHEMA
-    //TELL THE MODEL TO GIVE IT A CONFIDENCE RATING FROM 0-1(0.1, etc)
-    ///RETURN THE RESULTS TO THE USER
+  const files = req.files as Express.Multer.File[];
 
-    //clear the buffer
+  if (data.privacyMode === "true") {
+    if (!data.paymentMethods || !data.categories) {
+      return res.status(400).json({
+        message: MESSAGES.BAD_REQUEST,
+      });
+    }
+
+    const fileContents: FilePart[] = files.map((file) => {
+      return { type: "file", filename: file.filename, mediaType: file.mimetype, data: file.buffer };
+    });
+
+    const paymentMethods: Array<TextPart> = data.paymentMethods.map((paymentMethod) => {
+      return {
+        type: "text",
+        text: `Payment Method: { description: ${paymentMethod.description}, id: ${paymentMethod.id}}`,
+      };
+    });
+
+    const categories: Array<TextPart> = data.categories.map((category) => {
+      return {
+        type: "text",
+        text: `Category: {description: ${category.description}, id: ${category.id}}`,
+      };
+    });
+
+    const receiptParser = new ReceiptParser();
+    const { object, finishReason, warnings, usage, timeTaken } = await receiptParser.parse({
+      categories,
+      paymentMethods,
+      files: fileContents,
+    });
+
+    logger.info(MESSAGES.AI_GENERATION_USAGE, {
+      usage,
+      timeTaken,
+      url: req.url,
+      userId: req.user.id,
+      privacyMode: data.privacyMode,
+      requestId: req.headers["request-id"],
+    });
+
+    if (warnings?.length) {
+      logger.warn(MESSAGES.AI_GENERATION_WARNINGS, {
+        warnings,
+        url: req.url,
+        userId: req.user.id,
+        privacyMode: data.privacyMode,
+        requestId: req.headers["request-id"],
+      });
+    }
+
+    if (finishReason !== "stop") {
+      logger.warn(MESSAGES.AI_GENERATION_ENDED, {
+        url: req.url,
+        userId: req.user.id,
+        reason: finishReason,
+        privacyMode: data.privacyMode,
+        requestId: req.headers["request-id"],
+      });
+
+      return res.status(500).json({
+        message: MESSAGES.INTERNAL_SERVER_ERROR,
+      });
+    }
+
     clearBuffer(req);
 
-    return res.status(200).json({});
+    return res.status(200).json({
+      logs: object,
+      userId: req.user.id,
+    });
   }
 
+  //non-privacy users
   const batchId = uuid();
 
   const imageKeys: string[] = [];
-
-  const files = req.files as Express.Multer.File[];
 
   const imagesToUpload = files.map((file) => {
     const fileExtension = file.originalname.split(".").pop();
