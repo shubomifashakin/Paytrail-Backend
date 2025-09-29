d80dd3edac4fb928f27181a9cc46f8f3a039c017
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-09-29 17:54:28 +0100

    refactor

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,31 +1,5 @@
-jest.mock("./src/serverEnv", () => {
-  return {
-    port: "3000",
-    redis: "redis://localhost:6379",
-    googleClientId: "test-google-client-id",
-    googleClientSecret: "test-google-client-secret",
-    environment: "production",
-    databaseUrl: "postgresql://test:test@localhost:5432/test",
-    resend: "test-resend-key",
-    logLevel: "info",
-    serviceName: "paytrail-test",
-    otelExporterEndpoint: "http://localhost:4318",
-    signozIngestionKey: "test-signoz-key",
-    appScheme: "paytrail://test",
-    baseUrl: "http://localhost:3000",
-    paytrailAWSRegion: "us-east-1",
-    paytrailAWSAccessKey: "test-access-key",
-    paytrailAWSSecretKey: "test-secret-key",
-    paytrailStatementSqsQueueUrl: "https://sqs.us-east-1.amazonaws.com/test-queue",
-    receiptsBucketName: "test-receipts-bucket",
-    androidPlatformApplicationArn: "arn:aws:sns:us-east-1:123456789:test-android",
-    iosPlatformApplicationArn: "arn:aws:sns:us-east-1:123456789:test-ios",
-    broadcastTopicArn: "arn:aws:sns:us-east-1:123456789:test-broadcast",
-    userNotificationsTableARN: "arn:aws:dynamodb:us-east-1:123456789:table/test-user-notifications",
-    broadcastNotificationsTableARN:
-      "arn:aws:dynamodb:us-east-1:123456789:table/test-broadcast-notifications",
-  };
-});
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env.test" });
 
 jest.mock("resend", () => ({
   Resend: jest.fn(),
