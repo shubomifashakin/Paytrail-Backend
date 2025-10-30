7a1de7f488db115cdfd78bad7f838ff5b3c124b6
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-10-30 12:51:35 +0100

    added unique constrain on userid and period column

---
+++ prisma/migrations/20251030115112_unique_constrain_on_user_id_period_on_budgets_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[userId,period]` on the table `budgets` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateIndex
+CREATE UNIQUE INDEX "budgets_userId_period_key" ON "public"."budgets"("userId", "period");
