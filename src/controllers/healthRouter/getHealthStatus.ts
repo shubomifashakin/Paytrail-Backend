b4e0d690f74f4663d2e6d5fa7935a6d4a3ed03e8
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-12-06 21:46:42 +0100

    updated health endpoint response

--- src/controllers/healthRouter/getHealthStatus.ts
+++ src/controllers/healthRouter/getHealthStatus.ts
@@ -3,6 +3,6 @@ import { Request, Response } from "express";
 export async function getHealthStatus(_: Request, res: Response) {
   return res.status(200).json({
     message: "Healthy",
-    time: new Date().toDateString(),
+    time: new Date().toLocaleString(),
   });
 }
