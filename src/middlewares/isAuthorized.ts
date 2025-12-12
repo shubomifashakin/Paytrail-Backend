4dbb9abbf899d9b270d64cbce825a36fec120006
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-12-12 13:37:55 +0100

    added sliding window session extension

--- src/middlewares/isAuthorized.ts
+++ src/middlewares/isAuthorized.ts
@@ -2,8 +2,8 @@ import { NextFunction, Request, Response } from "express";
 
 import prisma from "../lib/prisma";
 
-import { MESSAGES } from "../utils/constants";
 import { logUnauthenticatedWarning } from "../utils/fns";
+import { MESSAGES, SESSION_EXPIRY_MS } from "../utils/constants";
 
 async function isAuthorized(req: Request, res: Response, next: NextFunction) {
   const sessionId = req.headers["authorization"]?.split(" ")[1];
@@ -35,6 +35,18 @@ async function isAuthorized(req: Request, res: Response, next: NextFunction) {
     });
 
     return res.status(401).json({ message: MESSAGES.UNAUTHORIZED });
+  }
+
+  const now = new Date();
+  const fourDaysInMs = 4 * 24 * 60 * 60 * 1000;
+  const timeUntilExpiry = session.expiresAt.getTime() - now.getTime();
+
+  if (timeUntilExpiry < fourDaysInMs) {
+    const newExpiry = new Date(now.getTime() + SESSION_EXPIRY_MS);
+    await prisma.session.update({
+      where: { id: sessionId },
+      data: { expiresAt: newExpiry },
+    });
   }
 
   req.user = session.user;
